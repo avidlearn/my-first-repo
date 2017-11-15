8347a5253e73ae77cec0c3e6fa66412aa33c9339
avidlearn <archkulo@outlook.com>
2017-11-16 02:33:26 +0800

    Updated with function

--- guessinggame.sh
+++ guessinggame.sh
@@ -2,6 +2,10 @@
 
 # Determine number of files within current directory and assign it to a variable by the name of number_to_be_guessed.
 
+function congrats { 
+	echo "Congratulations!!! You guessed correctly and this program shall end now - goodbye!"
+}
+
 number_to_be_guessed=$(ls | wc -l)
 
 index=0     # Set index to zero for counting number of user attempts.
@@ -28,16 +32,13 @@ do
      then 
         echo "You have made "$index" attempt(s) at guessing the number of files in the current directory."
         echo "Your guess is incorrect and is too high, please try again."
+fi
         
-  fi
-
-
   if [[ $INPUT -eq $number_to_be_guessed ]]
-
-     then echo "Congratulations!!! You guessed correctly after "$index" attempt(s) and this program shall end now - goodbye!"
-          echo "Program Ended."
-
+     then
+	echo "You have made "$index" attempt(s)."
+	congrats
      exit
   fi
 
-done 
+done
